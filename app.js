fab0c276dda81b7317b2dc782d28743a486add8b
seanfargose <102177453+seanfargose@users.noreply.github.com>
2023-07-27 15:56:38 +0530

    ADD ENVIRONMENT VARS

---
+++ app.js
@@ -0,0 +1,104 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption')
+
+const app = express();
+
+app.use(express.static("public"));
+
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/UserDB",{useNewUrlParser: true},
+).catch((err) => {console.error(err);})
+.then(()=>console.log("Connected to MongoDB"));
+
+var userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
+
+const User = new mongoose.model("User",userSchema);
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.get('/register', function(req, res){
+    res.render('register');
+});
+
+app.post("/register",function(req,res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+// newUser.save(function(err){
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         res.render("secrets");
+//     }
+// });
+
+newUser.save().then(()=>res.render('secrets'));
+});
+
+// app.post("/login",function(req,res){
+//     const username = req.body.username;
+//     const password = req.body.password;
+
+//     User.findOne({email: username ,function(err, foundUser){
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             if (foundUser){
+//                 if (password.foundUser === password){
+//                     res.render("secrets");
+//                 }
+//             }
+//         }
+//     }
+// });
+// });
+
+app.post("/login", async function(req, res){
+    try {
+        // check if the user exists
+        const user = await User.findOne({ email: req.body.username });
+        if (user) {
+          //check if password matches
+          const result = req.body.password === user.password;
+          if (result) {
+            res.render("secrets");
+          } else {
+            res.status(400).json({ error: "password doesn't match" });
+          }
+        } else {
+          res.status(400).json({ error: "User doesn't exist" });
+        }
+      } catch (error) {
+        res.status(400).json({ error });
+      }
+});
+
+
+
+
+
+app.listen(3000,function(){
+    console.log('server listening on port 3000');
+});
